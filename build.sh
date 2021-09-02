1bc767aaf140518953bc8f1b16b562d352efdace
vishal <vishalbollu@users.noreply.github.com>
2021-09-02 01:39:45 -0400

    Add timeout to build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 docker container rm --force elevate-challenge-container
 docker build -t elevate-challenge:latest .
-docker run -d --name elevate-challenge-container -e WEB_CONCURRENCY=1 -e MAX_WORKERS=1 -e ELEVATE_USERNAME=$ELEVATE_USERNAME -e ELEVATE_PASSWORD=$ELEVATE_PASSWORD -p 9000:80 elevate-challenge:latest
+docker run -d --name elevate-challenge-container -e WEB_CONCURRENCY=1 -e MAX_WORKERS=1 -e ELEVATE_USERNAME=$ELEVATE_USERNAME -e ELEVATE_PASSWORD=$ELEVATE_PASSWORD -e ELEVATE_TIMEOUT=$ELEVATE_TIMEOUT -p 9000:80 elevate-challenge:latest
